ca106ed4c42ea1bc45e48e577bab07a3185a3f9f
drishti  manghnani <drishtimanghnani311@gmail.com>
2025-07-25 10:53:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Optionally handle form submit
+document.querySelector("form")?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for joining Shree Shyam Seva Sansthan!");
+});
